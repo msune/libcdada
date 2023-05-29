4f550a70d2ef826d576150cf492c694bb8b9d277
Paolo Lucente <pl+github@pmacct.net>
2023-05-29 01:17:09 +0000

    ci: update to Ubuntu 22.04
    This commit:
    
    * Moves base image to Ubuntu 22.04, since 18.04 is no longer supported
      by actions
    * It uses containers to build with old containers (e.g. using 18.04)
    
    Signed-off-by: Paolo Lucente <pl+github@pmacct.net>
    Signed-off-by: Marc Sune <marcdevel@gmail.com>

--- .github/workflows/deps.sh
+++ .github/workflows/deps.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+if [ "${1}" == "gcc" ]; then
+	COMPILER="gcc-${2} g++-${2}"
+elif [ "${CC}" == "clang" ]; then
+	COMPILER="clang"
+else
+	echo "ERROR: invalid compiler version"
+	exit 1
+fi
+
+(sudo --help > /dev/null 2>&1 ) || (apt update && apt install -y sudo software-properties-common automake autoconf libtool make) #containers
 sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
 sudo apt-get update
-sudo apt-get install -y python3 valgrind gcc-4.8 g++-4.8 gcc-5 g++-5 gcc-6 g++-6 gcc-8 g++-8 gcc-9 g++-9 gcc-10 g++-10 clang
+sudo apt-get install -y python3 valgrind $COMPILER
